4a97e11ccbf7054f26400a170f0d18c6928d4790
Maxwell Ihiaso <ihiasomaxwellchukwuebuka@gmail.com>
2023-05-01 17:09:28 +0100

    Add home screen UI
    Add custom useFetch hook
    Add utils to check broken image link
    Add UI for welcome
    Add UI fro popular jobs
    Add UI for nearby jobs
    Add include custom fonts

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Redirect } from "expo-router";
 
-const Home = () => {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
+export default function Index() {
+    return <Redirect href="/home" />;
 }
-
-export default Home
-
-const styles = StyleSheet.create({})
---
+++ hook/useFetch.js
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { RapidAPI_Key, RapidAPI_Host } from "@env";
+
+const RAPID_API_KEY = RapidAPI_Key;
+const RAPID_API_HOST = RapidAPI_Host;
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://${RAPID_API_HOST}/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": RAPID_API_KEY,
+      "X-RapidAPI-Host": RAPID_API_HOST,
+    },
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+    } catch (error) {
+      if (!error.response) {
+        setError("Network Error");
+        console.log("Network Error");
+      } else {
+        setError(error);
+        console.log(error);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
