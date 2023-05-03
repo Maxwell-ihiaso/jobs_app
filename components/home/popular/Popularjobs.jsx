a21d9e426554e4cbf8d5cc24a7a3d13e1f068135
Maxwell Ihiaso <ihiasomaxwellchukwuebuka@gmail.com>
2023-05-03 18:46:43 +0100

    Add reload to popular and nearby jobs in the event of error

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -6,16 +6,17 @@ import {
   TouchableOpacity,
   FlatList,
   ActivityIndicator,
+  Image,
 } from "react-native";
 
 import styles from "./popularjobs.style";
-import { COLORS, SIZES } from "../../../constants";
+import { COLORS, SIZES, icons } from "../../../constants";
 import PopularJobCard from "../../common/cards/popular/PopularJobCard";
 import useFetch from "../../../hook/useFetch";
 
 const Popularjobs = () => {
   const router = useRouter();
-  const { data, isLoading, error } = useFetch("search", {
+  const { data, isLoading, error, refetch } = useFetch("search", {
     query: "React developer",
     num_pages: "1",
   });
@@ -40,7 +41,19 @@ const Popularjobs = () => {
         {isLoading ? (
           <ActivityIndicator size='large' color={COLORS.primary} />
         ) : error ? (
-          <Text>Something went wrong</Text>
+          <View style={styles.errorContainer}>
+            <Text>Something went wrong</Text>
+            <TouchableOpacity
+            style={styles.reloadBtnContainer}
+            onPress={() => refetch()}
+            >
+             <Image
+             source={icons.reload}
+             resizeMode="contain"
+             style={styles.reloadBtnImage}
+             />
+            </TouchableOpacity>
+          </View>
         ) : (
           <FlatList
             data={data}
