fa8386ec4bd50249cf37eb7b1856b61fa4de8f40
Maxwell Ihiaso <ihiasomaxwellchukwuebuka@gmail.com>
2023-05-17 23:18:45 +0100

    env update

--- app.config.js
+++ app.config.js
@@ -9,9 +9,6 @@ export default {
     name: "jobs_app",
     slug: "stay-hired",
     version: "1.0.4",
-    orientation: "portrait",
-    userInterfaceStyle: "automatic",
-    assestBundlePatterns: ["**/*"],
     extra: {
       eas: {
         projectId: "d61e2764-78ab-4258-a29e-4c5a869e4fd8",
@@ -25,16 +22,10 @@ export default {
     },
     updates: {
       url: "https://u.expo.dev/d61e2764-78ab-4258-a29e-4c5a869e4fd8",
-      fallbackToCacheTimeout: 0,
     },
     android: {
       package: "com.maxic_gray.stayhired",
-      versionCode: 4,
-      adaptiveIcon: {
-        foregroundImage:
-          "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
-        backgroundColor: "#ffffff",
-      },
+      versionCode: 4
     },
     ios: {
       bundleIdentifier: "-stayHired",
@@ -48,13 +39,6 @@ export default {
           android: {
             enableProguardInReleaseBuilds: true,
           },
-        },
-      ],
-      [
-        "expo-location",
-        {
-          locationAlwaysAndWhenInUsePermission:
-            "Allow $(PRODUCT_NAME) to use your location.",
         },
       ],
     ],
